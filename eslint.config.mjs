75e10fccfaa839239f978705b24308eea4c609ff
Cristian Cortes <cristian.cortes@klog.co>
2025-05-01 00:01:45 -0400

    fix(linter): fixed linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,23 @@
+// eslint.config.mjs
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "arrow-body-style": ["error", "as-needed"],
+      "prefer-arrow-callback": "error",
+    }
+  },
   pluginReact.configs.flat.recommended,
+  prettierConfig,
 ]);
